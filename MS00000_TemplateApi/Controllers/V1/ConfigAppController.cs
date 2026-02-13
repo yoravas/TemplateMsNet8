6f7e5ea3a2a73906750f8c54ce4c8241779096c7
yoravas <yoravas@gmail.com>
2026-02-13 09:47:04 +0100

    fix

--- MS00000_TemplateApi/Controllers/V1/ConfigAppController.cs
+++ MS00000_TemplateApi/Controllers/V1/ConfigAppController.cs
@@ -1,7 +1,6 @@
 ﻿using Asp.Versioning;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using MS00000_TemplateApi.Customizations.Attributes;
 
 using MS00000_TemplateApi.Model.Application;
 using MS00000_TemplateApi.Model.Application.DTOs;
@@ -11,11 +10,20 @@ namespace MS00000_TemplateApi.Controllers.V1;
 [Route("api/v{version:apiVersion}/[controller]")]
 [ApiController]
 [ApiVersion("1.0")]
+[ProducesResponseType(typeof(ApiResponse<ReturnDetails>), StatusCodes.Status406NotAcceptable)]
+[ProducesResponseType(typeof(ApiResponse<ReturnDetails>), StatusCodes.Status404NotFound)]
+[ProducesResponseType(typeof(ApiResponse<ReturnDetails>), StatusCodes.Status401Unauthorized)]
+[ProducesResponseType(typeof(ApiResponse<ReturnDetails>), StatusCodes.Status415UnsupportedMediaType)]
+[ProducesResponseType(typeof(ApiResponse<ReturnDetails>), StatusCodes.Status400BadRequest)]
+[ProducesResponseType(typeof(ApiResponse<ReturnDetails>), StatusCodes.Status500InternalServerError)]
+[ProducesResponseType(typeof(ApiResponse<ReturnDetails>), StatusCodes.Status502BadGateway)]
+[ProducesResponseType(typeof(ApiResponse<ReturnDetails>), StatusCodes.Status503ServiceUnavailable)]
+[ProducesResponseType(typeof(ApiResponse<ReturnDetails>), StatusCodes.Status406NotAcceptable)]
 public class ConfigAppController(IMediator mediator, ILogger<ConfigAppController> logger) : ApiBaseController
 {
+
     [HttpGet]
     [ProducesResponseType(typeof(ApiResponse<List<ConfigAppDto>>), StatusCodes.Status200OK)]
-    [ResponseOpenApi]
     [Route("GetConfigApp")]
     [Produces("application/json")]
     public async Task<IActionResult> GetConfigAppAsync()
@@ -37,3 +45,4 @@ public class ConfigAppController(IMediator mediator, ILogger<ConfigAppController
 
     }
 }
+
--- MS00000_TemplateApi/Customizations/Middlewares/CorrelationIdMiddleware.cs
+++ MS00000_TemplateApi/Customizations/Middlewares/CorrelationIdMiddleware.cs
@@ -33,5 +33,8 @@ public class CorrelationIdMiddleware : IMiddleware
         //});
 
         await next(context);
+
+
+
     }
 }
